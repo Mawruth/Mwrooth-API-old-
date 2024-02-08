fe6fba5d1a5a2d7ed2e161655784cbe2755b3d5f
PandaX185 <mouss3abbb@gmail.com>
2024-02-09 00:44:23 +0200

    add token verification middleware

--- middlewares/middleware.go
+++ middlewares/middleware.go
@@ -1,1 +1,53 @@
 package middlewares
+
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
+	"os"
+	"strings"
+)
+
+var ExceptionEndpoints = []string{
+	"/api/v1/users/register",
+	"/api/v1/users/login",
+	"/api/v1/users/otp",
+}
+
+func CheckAccessToken(c *fiber.Ctx) error {
+	for _, exc := range ExceptionEndpoints {
+		if strings.Contains(c.Path(), exc) {
+			return c.Next()
+		}
+	}
+	token := c.Get("Authorization")
+	token = strings.ReplaceAll(token, "Bearer ", "")
+	if token == "" {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "Unauthorized",
+		})
+	}
+	if err := verifyToken(token); err != nil {
+		fmt.Println(token)
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+	return c.Next()
+}
+
+func verifyToken(tokenString string) error {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("SECRET_KEY")), nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	if !token.Valid {
+		return fmt.Errorf("invalid token")
+	}
+
+	return nil
+}
