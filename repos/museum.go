f11563258b6e5fd31d9d1be6b34276377ae42833
Mohamed Elsharaky <101217349+Elsharaky@users.noreply.github.com>
2024-02-09 09:26:50 +0200

    Implemented searching functionality (#7)

--- repos/museum.go
+++ repos/museum.go
@@ -28,3 +28,26 @@ func (r *MuseumRepository) GetAll() ([]*models.Museum, error) {
 	var museums []*models.Museum
 	return museums, r.db.Preload("Types").Preload("Images").Find(&museums).Error
 }
+
+func (r *MuseumRepository) GetByRating(rating float32) ([]*models.Museum, error) {
+	var museums []*models.Museum
+	return museums, r.db.Where("rating = ?", rating).Find(&museums).Error
+}
+
+func (r *MuseumRepository) GetByTypes(types []int) ([]*models.Museum, error) {
+	var types_ []*models.Type
+	err := r.db.Where("id IN ?", types).Preload("Museums").Find(&types_).Error
+	if err != nil {
+		return nil, err
+	}
+	var museums []*models.Museum
+	for _, t := range types_ {
+		museums = append(museums, t.Museums...)
+	}
+	return museums, nil
+}
+
+func (r *MuseumRepository) GetByCity(city string) ([]*models.Museum, error) {
+	var museums []*models.Museum
+	return museums, r.db.Where("city = ?", city).Find(&museums).Error
+}
